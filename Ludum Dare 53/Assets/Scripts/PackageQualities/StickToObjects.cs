1d23795f9cddebee95c21fca0750d45136db6908
JasonReff <jmr8382@rit.edu>
2023-04-29 15:53:17 -0400

    The stickiest commit in history

---
+++ Ludum Dare 53/Assets/Scripts/PackageQualities/StickToObjects.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(FixedJoint2D))]
+public class StickToObjects : MonoBehaviour
+{
+    private FixedJoint2D _fixedJoint;
+    private Rigidbody2D _rigidbody;
+    private float _maximumVelocity = 50f;
+
+    private void Awake()
+    {
+        _fixedJoint = GetComponent<FixedJoint2D>();
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _fixedJoint.enabled = false;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.TryGetComponent(out PackageQualities packageQualities))
+        {
+            _fixedJoint.enabled = true;
+            _fixedJoint.connectedBody = collision.rigidbody;
+        }
+    }
+
+    private void Update()
+    {
+        if (_fixedJoint.enabled &&_rigidbody.velocity.sqrMagnitude > _maximumVelocity)
+        {
+            _fixedJoint.connectedBody = null;
+            _fixedJoint.enabled = false;
+        }
+    }
+
+
+}
---
+++ Ludum Dare 53/Assets/Scripts/PackageQualities/StickyQuality.cs
@@ -0,0 +1,8 @@
+﻿public class StickyQuality : LayeredQuality
+{
+    public override void ShowQuality(PackageQualities package)
+    {
+        base.ShowQuality(package);
+        package.gameObject.AddComponent<StickToObjects>();
+    }
+}
