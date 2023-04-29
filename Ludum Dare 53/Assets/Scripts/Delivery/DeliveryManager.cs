1c6f8a8540e4c386903ba51cc9da2ddcbbd4ebb8
JasonReff <jmr8382@rit.edu>
2023-04-29 14:57:59 -0400

    Fixed package layering and added spawner

--- Ludum Dare 53/Assets/Scripts/Delivery/DeliveryManager.cs
+++ Ludum Dare 53/Assets/Scripts/Delivery/DeliveryManager.cs
@@ -1,22 +1,48 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour
 {
     [SerializeField] private Qualities _desiredPackageQualities;
-    [SerializeField] private int _startingLives = 3;
+    [SerializeField] private int _startingLives = 3, _minimumPackages = 3;
+    [SerializeField] private PackageSpawner _spawner;
+    private List<PackageQualities> _allPackages = new List<PackageQualities>();
     private int _currentLives;
     private int _points;
+
+    public List<PackageQualities> AllPackages { get => _allPackages; }
 
     private void Start()
     {
         _currentLives = _startingLives;
     }
 
-    private void ComparePackage(PackageQualities package)
+    private void OnEnable()
     {
-        if (package.Qualities == _desiredPackageQualities)
-        { 
-        
+        Deliverable.OnPackageDelivered += OnDelivery;
+    }
+
+    private void OnDisable()
+    {
+        Deliverable.OnPackageDelivered -= OnDelivery;
+    }
+
+    private void OnDelivery(Deliverable deliverable)
+    {
+        if (deliverable.TryGetComponent(out PackageQualities package))
+        {
+            _allPackages.Remove(package);
+            ComparePackage(deliverable);
+        }
+        if (_allPackages.Count < _minimumPackages)
+            _spawner.SpawnPackages();
+    }
+
+    private void ComparePackage(Deliverable deliverable)
+    {
+        if (deliverable.GetComponent<PackageQualities>().Qualities == _desiredPackageQualities)
+        {
+            DeliverySuccessful(deliverable);
         }
         else
         {
