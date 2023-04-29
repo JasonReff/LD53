4de4e8cc824dd227c3724c88c89193b2c47a61dc
JasonReff <jmr8382@rit.edu>
2023-04-29 14:24:28 -0400

    Separate overlapping packages

---
+++ Ludum Dare 53/Assets/Scripts/SeparateOnCollision.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class SeparateOnCollision : MonoBehaviour
+{
+    [SerializeField] private float _bounceForce = 5f;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Vector2 distanceVector = transform.position - collision.otherCollider.transform.position;
+        collision.otherRigidbody.AddForce(-distanceVector * _bounceForce);
+        collision.rigidbody.AddForce(distanceVector * _bounceForce);
+    }
+}
