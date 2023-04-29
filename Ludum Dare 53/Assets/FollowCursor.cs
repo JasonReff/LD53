f4436f4ae4bd32b3aa5fb458cde90195e285d58a
JasonReff <jmr8382@rit.edu>
2023-04-29 10:36:03 -0400

    Fun draggies

--- Ludum Dare 53/Assets/FollowCursor.cs
+++ Ludum Dare 53/Assets/FollowCursor.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class FollowCursor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private TargetJoint2D _targetJoint2D;
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _maxForce = 10;
+    private Vector2 _velocity;
+
+
+    private void OnMouseDown()
     {
-        
+        _targetJoint2D.enabled = true;
+        _targetJoint2D.anchor = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnMouseDrag()
     {
-        
+        _targetJoint2D.target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _velocity = _rigidbody.velocity;
+    }
+
+    private void OnMouseUp()
+    {
+        _targetJoint2D.enabled = false;
+        _rigidbody.AddForce(Vector2.ClampMagnitude(_velocity, _maxForce));
     }
 }
