59fa6c8373e01509c93e755c9026ae68e9464ca4
JasonReff <jmr8382@rit.edu>
2023-04-29 15:10:36 -0400

    Tested spawner and added gizmos

--- Ludum Dare 53/Assets/Scripts/Delivery/PackageSpawner.cs
+++ Ludum Dare 53/Assets/Scripts/Delivery/PackageSpawner.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 public class PackageSpawner : MonoBehaviour
 {
@@ -7,6 +8,11 @@ public class PackageSpawner : MonoBehaviour
     [SerializeField] private Vector2 _bottomLeftSpawningCorner, _topRightSpawningCorner;
     [SerializeField] private DeliveryManager _deliveryManager;
     [SerializeField] private int _minimumLoad = 5, _maximumLoad = 9;
+
+    private void Start()
+    {
+        SpawnPackages();
+    }
 
     public void SpawnPackages()
     {
@@ -25,5 +31,19 @@ public class PackageSpawner : MonoBehaviour
         var qualities = _pool.RandomQualities();
         package.ShowQualities(qualities);
         _deliveryManager.AllPackages.Add(package);
+    }
+
+    public void OnDrawGizmosSelected()
+    {
+        var points = new List<Vector2>() { _bottomLeftSpawningCorner,
+            new Vector2(_bottomLeftSpawningCorner.x, _topRightSpawningCorner.y),
+            _topRightSpawningCorner,
+            new Vector2(_topRightSpawningCorner.x, _bottomLeftSpawningCorner.y)};
+        foreach (var point in points)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawSphere(point, 0.25f);
+            Gizmos.DrawLine(point, points.GetNext(point));
+        }
     }
 }
