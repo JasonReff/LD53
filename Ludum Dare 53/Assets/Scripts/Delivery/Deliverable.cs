cc1679f4296eda5540a209887cd7baed5a387597
JasonReff <jmr8382@rit.edu>
2023-04-29 11:13:28 -0400

    Delivery Manager

--- Ludum Dare 53/Assets/Scripts/Delivery/Deliverable.cs
+++ Ludum Dare 53/Assets/Scripts/Delivery/Deliverable.cs
@@ -1,10 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class Deliverable : MonoBehaviour
 {
     private bool _delivered;
     [SerializeField] private int _pointsForDelivering;
+
+    public int Points { get => _pointsForDelivering; }
     public static Action<Deliverable> OnPackageDelivered;
 
     public void DeliverPackage()
---
+++ Ludum Dare 53/Assets/Scripts/Delivery/DeliveryManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour
+{
+    [SerializeField] private Qualities _desiredPackageQualities;
+    [SerializeField] private int _startingLives = 3;
+    private int _currentLives;
+    private int _points;
+
+    private void Start()
+    {
+        _currentLives = _startingLives;
+    }
+
+    private void ComparePackage(PackageQualities package)
+    {
+        if (package.Qualities == _desiredPackageQualities)
+        { 
+        
+        }
+        else
+        {
+            LoseLife();
+        }
+    }
+
+    private void DeliverySuccessful(Deliverable deliverable)
+    {
+        _points += deliverable.Points;
+    }
+
+    private void LoseLife()
+    {
+        _currentLives--;
+        if (_currentLives <= 0)
+        {
+            GameOver();
+        }
+    }
+
+    private void GameOver()
+    {
+
+    }
+
+}
