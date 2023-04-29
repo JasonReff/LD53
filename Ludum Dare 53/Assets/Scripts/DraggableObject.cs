82797d8713d765b4d3686d27f9c4c67ab2e25658
JasonReff <jmr8382@rit.edu>
2023-04-29 10:11:29 -0400

    spring drag

--- Ludum Dare 53/Assets/Scripts/DraggableObject.cs
+++ Ludum Dare 53/Assets/Scripts/DraggableObject.cs
@@ -7,13 +7,18 @@ public class DraggableObject : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _minimumDragTime = 0.1f, _maximumForce = 10f;
-    private Vector2 _startingPosition, _endingPosition;
+    private Vector2 _pivot, _startingPosition, _endingPosition;
     private float _timer = 0f;
     private Coroutine _dragCoroutine;
 
+    private void Start()
+    {
+        
+    }
 
     private void OnMouseDown()
     {
+        _pivot = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         _dragCoroutine = StartCoroutine(DragCoroutine());
     }
 
@@ -25,6 +30,11 @@ public class DraggableObject : MonoBehaviour
         ThrowObject();
     }
 
+    private void OnMouseDrag()
+    {
+        transform.position = ((Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - _pivot);
+    }
+
     private IEnumerator DragCoroutine()
     {
         _timer = 0f;
@@ -32,7 +42,7 @@ public class DraggableObject : MonoBehaviour
         while (_timer < _minimumDragTime)
         {
             _timer += Time.deltaTime;
-            yield return new WaitForSeconds(Time.deltaTime);
+            yield return null;
         }
         _endingPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         _dragCoroutine = StartCoroutine(DragCoroutine());
