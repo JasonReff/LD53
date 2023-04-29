d50d732f940a4084cbc95292332f91806fb5ccb7
JasonReff <jmr8382@rit.edu>
2023-04-29 16:08:25 -0400

    Sticky on multiple layers

--- Ludum Dare 53/Assets/Scripts/PackageQualities/StickToObjects.cs
+++ Ludum Dare 53/Assets/Scripts/PackageQualities/StickToObjects.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 
 [RequireComponent(typeof(FixedJoint2D))]
 public class StickToObjects : MonoBehaviour
@@ -25,11 +26,38 @@ public class StickToObjects : MonoBehaviour
 
     private void Update()
     {
+        if (_fixedJoint.enabled == false)
+        {
+            StickToObjectsInOtherLayers();
+        }
         if (_fixedJoint.enabled &&_rigidbody.velocity.sqrMagnitude > _maximumVelocity)
         {
             _fixedJoint.connectedBody = null;
             _fixedJoint.enabled = false;
         }
+    }
+
+    private void StickToObjectsInOtherLayers()
+    {
+        var layers = new List<LayerMask>() { LayerMask.NameToLayer("Truck1"), LayerMask.NameToLayer("Truck2"), LayerMask.NameToLayer("Truck3") };
+        var filter = new ContactFilter2D();
+        var results = new List<Collider2D>();
+        foreach (var layer in layers)
+        {
+            filter.layerMask = layer;
+            GetComponent<PolygonCollider2D>().OverlapCollider(filter, results);
+            foreach (var result in results)
+            {
+                if (result.TryGetComponent(out PackageQualities package))
+                {
+                    _fixedJoint.enabled = true;
+                    _fixedJoint.connectedBody = results[0].attachedRigidbody;
+                }
+            }
+            
+                
+        }
+        
     }
 
 
